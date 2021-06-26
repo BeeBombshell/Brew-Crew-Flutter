ab43faf7eb8323b26420987b10c794be0412025e
Bhavya Verma <vbhavya269@gmail.com>
2021-06-26 13:40:45 +0530

    Initialized Flutter Application and added Home and Authenticate Screens

---
+++ lib/screens/wrapper.dart
@@ -0,0 +1,10 @@
+import 'package:brew_crew/screens/home/home.dart';
+import 'package:flutter/material.dart';
+
+class Wrapper extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // return either Home or Authenticate
+    return Home();
+  }
+}
